a5442b1196ff7f48a960dc245a69b434f61c8293
David Buser <busdav@outlook.com>
2018-05-13 19:40:49 +0200

    Wiki CRUD: mapped 'Rails CRUD' up to 'Styling the new view', but #create not working

--- spec/controllers/wikis_controller_spec.rb
+++ spec/controllers/wikis_controller_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
-# include Devise::Test::ControllerHelpers
 
 RSpec.describe WikisController, type: :controller do
 
   let(:my_wiki) { create(:wiki) }
+  let(:user) { create(:user) }
+
 
 
   context "anonymous user" do
@@ -14,7 +15,6 @@ RSpec.describe WikisController, type: :controller do
     end
 
     describe "GET index" do
-
       it "should be redirected to signin" do
         get :index
         expect( response ).to redirect_to( new_user_session_path )
@@ -23,22 +23,23 @@ RSpec.describe WikisController, type: :controller do
   end
 
 
+
   context "logged-in user" do
 
     before :each do
-      # This simulates an anonymous user
       login_with create( :user )
     end
 
-    describe "GET index" do
-      it "should let a user see all the wikis" do
-        get :index
-        expect( response ).to render_template( :index )
-      end
 
+    describe "GET index" do
       it "returns http success" do
         get :index
         expect(response).to have_http_status(:success)
+      end
+
+      it "renders the #index view" do
+        get :index
+        expect( response ).to render_template( :index )
       end
 
       it "assigns [my_wiki] to @wikis" do
@@ -46,6 +47,7 @@ RSpec.describe WikisController, type: :controller do
         expect(assigns(:wikis)).to eq([my_wiki])
       end
     end
+
 
     describe "GET new" do
       it "returns http success" do
@@ -58,27 +60,30 @@ RSpec.describe WikisController, type: :controller do
         expect(response).to render_template :new
       end
 
-      it "instantiates @post" do
+      it "instantiates @wiki" do
         get :new
-        expect(assigns(:post)).not_to be_nil
+        expect(assigns(:wiki)).not_to be_nil
       end
     end
+
 
     describe "POST create" do
-      it "increases the number of Post by 1" do
-        expect{ post :create, params: { post: { title: RandomData.random_sentence, body: RandomData.random_paragraph } } }.to change(Post,:count).by(1)
+      it "increases the number of Wiki by 1" do
+        expect{ post :create, params: { wiki: { title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user } } }.to change(Wiki,:count).by(1)
       end
 
-      it "assigns the new post to @post" do
-        post :create, params: { post: { title: RandomData.random_sentence, body: RandomData.random_paragraph } }
-        expect(assigns(:post)).to eq Post.last
+      it "assigns the new wiki to @wiki" do
+        post :create, params: { wiki: { title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user } }
+        expect(assigns(:wiki)).to eq Wiki.last
       end
 
-      it "redirects to the new post" do
-        post :create, params: { post: { title: RandomData.random_sentence, body: RandomData.random_paragraph } }
-        expect(response).to redirect_to Post.last
+      it "redirects to the new wiki" do
+        post :create, params: { wiki: { title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user } }
+        expect(response).to redirect_to Wiki.last
       end
     end
+
+
 
     describe "GET #show" do
       it "returns http success" do
