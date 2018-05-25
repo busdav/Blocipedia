20bd227c2a64b45131b7699c1693bdf9e2d5dced
David Buser <busdav@outlook.com>
2018-05-25 08:54:03 +0100

    User Roles: defaulted wikis to non-private

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,3 +1,11 @@
 class Wiki < ApplicationRecord
   belongs_to :user
+  after_initialize :default_to_public
+
+
+  private
+
+  def default_to_public
+    self.private = false if self.private.nil?
+  end
 end
