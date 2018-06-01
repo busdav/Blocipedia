3db3869dd4c785e5a940b766b9095bbf4950ab47
David Buser <busdav@outlook.com>
2018-06-01 09:10:59 +0100

    User roles: added prep thoughts on scope

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -4,11 +4,17 @@ class WikiPolicy < ApplicationPolicy
     user.present?
   end
 
+# probably add the same for edit, no? or, probably reference in appl policy made to 'update' is enough.
+
+
 
   class Scope < Scope
     def resolve
       scope
-      # replace with appropriate scope, eg: 
+      # replace with appropriate scope, eg below. Goal is that standard and premium users only sees public wikis. admin users see all wikis.
+      # probably have to put 'update' etc. under the scope??
+      # although no, I believe that scope and actions are TESTED individually. I then simply need to make sure I use "policy_scope" in the controller.
+      # That will make the respective action ONLY accessible to the appropriate scope. check.
       # if user.admin?
       #   scope.all
       # else
