5ff7ee6f35a34a6a5878446fbe528d1a59a87a12
David Buser <busdav@outlook.com>
2018-07-07 11:03:07 +0100

    Upgrading account: first attempt making private wikis public after downgrade

--- lib/change_user_role.rb
+++ lib/change_user_role.rb
@@ -10,5 +10,9 @@ module ChangeUserRole
     if current_user.role == "premium"
       current_user.standard!
     end
+    @wikis = Wiki.where(:user_id => current_user.id)
+    @wikis.each do |wiki|
+      wiki.private = false
+    end
   end
 end
